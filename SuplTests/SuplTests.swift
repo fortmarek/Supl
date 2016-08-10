deca6f32bde5a05dad25b021c1bdad94111515a3
fortmarek <marekfort@me.com>
2016-08-10 16:37:33 +0200

    tests

--- SuplTests/SuplTests.swift
+++ SuplTests/SuplTests.swift
@@ -7,23 +7,26 @@
 //
 
 import XCTest
+@testable import Supl
 
 class SuplTests: XCTestCase {
+    
+    var vc: ViewController!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let viewController = storyboard.instantiateViewControllerWithIdentifier("suplVC") as? ViewController else {return}
+        self.vc = viewController
+        
+        let _ = viewController.view
+        
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    
     }
     
     func testPerformanceExample() {
