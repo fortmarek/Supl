b2a62c628857a1b8808226c3bdbc1f9581a33a81
fortmarek <marekfort@me.com>
2016-09-14 23:03:29 +0200

    version bump to 2.1.5

--- Supl/SettingsViewController.swift
+++ Supl/SettingsViewController.swift
@@ -116,7 +116,7 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
             guard let _ = footer.textLabel else {return}
             footer.textLabel!.font = UIFont().getFont(14, weight: .light)
             footer.textLabel!.textAlignment = .center
-            footer.textLabel!.text = "Verze 2.1.4\nVytvořil Marek Fořt"
+            footer.textLabel!.text = "Verze 2.1.5\nVytvořil Marek Fořt"
         }
         
     }
